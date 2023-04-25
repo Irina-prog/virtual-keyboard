06ab17780d327292536ae3aab250d437705bf9d1
“Irina <ira.bel4ikova@gmail.com>
2023-04-25 21:05:59 +0400

    feat: added keyboard rendering

--- js/keyboard.js
+++ js/keyboard.js
@@ -9,17 +9,44 @@ export default class Keyboard {
 
   #layout = 'en';
 
+  #inputElementKeyDownBound = null;
+
+  #inputElementKeyUpBound = null;
+
   constructor(parent) {
     this.#parent = parent;
+    this.#inputElementKeyDownBound = this.#inputElementKeyDown.bind(this);
+    this.#inputElementKeyUpBound = this.#inputElementKeyUp.bind(this);
   }
 
   bind(inputElement) {
+    this.#inputElement?.removeEventListener('keydown', this.#inputElementKeyDownBound);
+    this.#inputElement?.removeEventListener('keyup', this.#inputElementKeyUpBound);
     this.#inputElement = inputElement;
+    this.#inputElement?.addEventListener('keydown', this.#inputElementKeyDownBound);
+    this.#inputElement?.addEventListener('keyup', this.#inputElementKeyUpBound);
   }
 
   show() {
+    if (this.#container) {
+      this.#container.remove();
+    }
     this.#container = document.createElement('div');
     this.#container.classList.add('keyboard-container');
+    const rows = layouts[this.#layout];
+    this.#container.append(...rows.map((row) => {
+      const rowElement = document.createElement('div');
+      rowElement.classList.add('keyboard-row');
+      rowElement.append(...row.map((key) => {
+        const keyElement = document.createElement('button');
+        keyElement.classList.add('keyboard-key');
+        keyElement.innerText = key.key ?? key;
+        keyElement.addEventListener('click', (e) => this.#type(key, e.shiftKey));
+        return keyElement;
+      }));
+      return rowElement;
+    }));
+    this.#parent.append(this.#container);
   }
 
   changeLayout(layout) {
@@ -28,5 +55,20 @@ export default class Keyboard {
     }
     this.#layout = layout;
     this.show();
+  }
+
+  #type(key, shiftKey) {
+    const keyToType = shiftKey ? (key.shift ?? key.toUpperCase()) : (key.key ?? key);
+    this.#inputElement.value += keyToType;
+  }
+
+  #inputElementKeyDown(e) {
+    console.log('KeyDown');
+    console.log(e);
+  }
+
+  #inputElementKeyUp(e) {
+    console.log('KeyUp');
+    console.log(e);
   }
 }
--- js/main.js
+++ js/main.js
@@ -9,3 +9,4 @@ function start() {
 }
 
 document.addEventListener('DOMContentLoaded', start);
+
